2422d341fc0a10b67d00f22c1a3d0f42f592e141
femi_ml^C <akinspajo@gmail.com^C>
2026-03-22 00:24:09 +0100

    add redshift security group to terraform

--- terraform/redshift.tf
+++ terraform/redshift.tf
@@ -4,7 +4,7 @@ locals {
   }
 }
 
-# IAM Role for Redshift
+# iam role for redshift to access s3
 resource "aws_iam_role" "redshift_role" {
   name = "dec-capstone-joshua-redshift-role"
 
@@ -12,9 +12,9 @@ resource "aws_iam_role" "redshift_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Sid       = ""
         Principal = {
           Service = "redshift.amazonaws.com"
         }
@@ -25,7 +25,7 @@ resource "aws_iam_role" "redshift_role" {
   tags = merge(local.redshift_tags, local.generic_tag)
 }
 
-# IAM Policy Document
+# iam policy document
 data "aws_iam_policy_document" "redshift_role_policy" {
   statement {
     sid = "S3ReadAndWrite"
@@ -41,40 +41,72 @@ data "aws_iam_policy_document" "redshift_role_policy" {
   }
 }
 
-# IAM Policy Resource
+# iam policy resource
 resource "aws_iam_policy" "redshift_policy" {
   name   = "dec-capstone-joshua-redshift-policy"
   policy = data.aws_iam_policy_document.redshift_role_policy.json
 }
 
-# Attach Policy to Role
+# attach policy to role
 resource "aws_iam_policy_attachment" "redshift_role_policy_attach" {
   name       = "dec-capstone-joshua-redshift-policy-attach"
   roles      = [aws_iam_role.redshift_role.name]
   policy_arn = aws_iam_policy.redshift_policy.arn
 }
 
-# Get Redshift username from SSM
+# get redshift username from ssm
 data "aws_ssm_parameter" "redshift_db_username" {
   name = "/dec-capstone-joshua/redshift/db_username"
 }
 
-# Get Redshift password from SSM
+# get redshift password from ssm
 data "aws_ssm_parameter" "redshift_db_password" {
   name = "/dec-capstone-joshua/redshift/db_password"
 }
 
-# Provision Redshift Cluster
+# get the default vpc
+data "aws_vpc" "default" {
+  default = true
+}
+
+# create a security group for redshift
+resource "aws_security_group" "redshift_sg" {
+  name        = "dec-capstone-joshua-redshift-sg"
+  description = "security group for redshift cluster"
+  vpc_id      = data.aws_vpc.default.id
+
+  # allow inbound traffic on redshift port from anywhere
+  ingress {
+    from_port   = 5439
+    to_port     = 5439
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "redshift public access"
+  }
+
+  # allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.generic_tag
+}
+
+# provision redshift cluster
 resource "aws_redshift_cluster" "main" {
-  cluster_identifier  = "dec-capstone-joshua-cluster"
-  database_name       = "dec_capstone_joshua"
-  master_username     = data.aws_ssm_parameter.redshift_db_username.value
-  master_password     = data.aws_ssm_parameter.redshift_db_password.value
-  node_type           = "ra3.large"
-  cluster_type        = "single-node"
-  publicly_accessible = true
-  skip_final_snapshot = true
-  iam_roles           = [aws_iam_role.redshift_role.arn]
+  cluster_identifier     = "dec-capstone-joshua-cluster"
+  database_name          = "dec_capstone_joshua"
+  master_username        = data.aws_ssm_parameter.redshift_db_username.value
+  master_password        = data.aws_ssm_parameter.redshift_db_password.value
+  node_type              = "ra3.large"
+  cluster_type           = "single-node"
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+  iam_roles              = [aws_iam_role.redshift_role.arn]
+  vpc_security_group_ids = [aws_security_group.redshift_sg.id]
 
   tags = merge(local.generic_tag, local.redshift_tags)
 }
